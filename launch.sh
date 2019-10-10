2fb1abc6d97105c484fa510e8bb035fa1d5206c9
saryazdi <soroush.saryazdi@gmail.com>
2019-10-10 11:22:37 -0400

    added pp lanefollowewr

--- launch.sh
+++ launch.sh
@@ -5,3 +5,4 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
+roslaunch pure_pursuit_lf pure_pursuit_lf.launch
